8eef941636234138337c06bae3729f9b5933519c
Prem Sharma <sonusharma4237@gmail.com>
2025-07-20 12:22:50 +0530

    First commit with phase1.py

---
+++ phase1.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+# x = [1, 2, 3, 4, 5]
+# y = [2, 3, 5, 7, 11]
+# plt.plot(x,y)
+# plt.show()
+x = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+y = [5, 7, 8, 6, 4]
+plt.title('Weekly sales')
+plt.xlabel('Days of the week')
+plt.ylabel('Number of sales')
+plt.plot(x, y, marker='o', color='b', linestyle='-')
+plt.show()
