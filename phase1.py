52383a715b64ba13521684fd0cbd5fbf9190927a
Prem Sharma <sonusharma4237@gmail.com>
2025-07-20 12:58:19 +0530

    bar

--- phase1.py
+++ phase1.py
@@ -3,10 +3,18 @@ import matplotlib.pyplot as plt
 # y = [2, 3, 5, 7, 11]
 # plt.plot(x,y)
 # plt.show()
-x = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+# x = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+# y = [5, 7, 8, 6, 4]
+# plt.title('Weekly sales')
+# plt.xlabel('Days of the week')
+# plt.ylabel('Number of sales')
+# plt.plot(x, y, marker='o', color='b', linestyle='-')
+# plt.show()
+
+x = ['A', 'B', 'C', 'D', 'E']
 y = [5, 7, 8, 6, 4]
-plt.title('Weekly sales')
-plt.xlabel('Days of the week')
-plt.ylabel('Number of sales')
-plt.plot(x, y, marker='o', color='b', linestyle='-')
+plt.title('Product Sales')
+plt.xlabel('Products')
+plt.ylabel('Number of Sales')
+plt.bar(x, y, color='blue')
 plt.show()
